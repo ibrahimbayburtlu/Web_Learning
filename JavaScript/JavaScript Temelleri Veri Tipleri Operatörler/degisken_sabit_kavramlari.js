0487e91d667986837dd1cc81b58bf1d66672ebca
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-04-08 18:07:02 +0300

    I am learning

---
+++ JavaScript/JavaScript Temelleri Veri Tipleri Operatörler/degisken_sabit_kavramlari.js
@@ -0,0 +1,29 @@
+// var let ve const ile değişken tanımlanabilir 
+// noktalı virgül kullanımı isteğe bağlıdır
+// değişken isimleri bazı kurallara göre verilir.
+
+var yasim = 33;
+console.log("yasim");
+console.log("yasim");
+console.log("yasim");
+console.log("yasim");
+console.log("yasim");
+console.log("yasim");
+
+let _sayi = 123456;
+let $sayi = 123456;
+let sayi2323 = 123456;
+let benimDogumYilim = 1988;
+let benimDogum_yilim = 2000;
+
+let sayi1;
+sayi1 = 50;
+sayi2 = 60;
+console.log(sayi1);
+
+const piSayisi = 3.14;
+// piSayisi = 3.15;
+
+// var eklemeden yazmamak hata değil ama önerilmez. 
+notum = 50;
+console.log(notum);
