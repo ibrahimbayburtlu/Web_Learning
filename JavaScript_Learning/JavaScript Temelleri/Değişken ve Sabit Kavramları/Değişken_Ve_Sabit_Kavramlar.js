898318301f3cada615af76be554df8ddfe68574b
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-04-26 22:56:52 +0300

    Değişken ve Sabit Kavramları | var, let , const Anahtar Kelimeleri

---
+++ JavaScript_Learning/JavaScript Temelleri/Değişken ve Sabit Kavramları/Değişken_Ve_Sabit_Kavramlar.js
@@ -0,0 +1,27 @@
+// var let ve const ile değişken tanımlanabilir.
+// noktalı virgül kullanımı isteğe bağlıdır.
+// değişken isimleri bazı kurallara göre verilir.
+
+var yasim = 31;
+console.log(yasim);
+
+let tcKimlik = 123456;
+let _sayi = 123456;
+let $sayi = 123456;
+let sayi2323 = 123456;
+let benimDogumYilim = 1988;
+let benim_dogum_yilim = 1988;
+let benim_dogumYilim = 1988;
+let  sayi1; 
+
+console.log(tcKimlik);
+console.log($sayi);
+
+// const sayisi hiçbir zaman değişmez. 
+const piSayisi = 3.14;
+
+console.log(piSayisi);
+
+
+let notum = 50;
+console.log(notum);
