61950a97359930a8c957519111df3414268052ea
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-04-28 20:43:57 +0300

    I'm learning console Alert ve Prompt Komutları

---
+++ JavaScript_Learning/JavaScript Temelleri/Console, Alert ve Prompt Komutları/console_alert_prompt.js
@@ -0,0 +1,30 @@
+let myAge = 31;
+console.log(myAge);
+// Hata vermek istersek 
+console.error('Yalnış işlem');
+// Uyarma işlemi yapmak istersek
+console.warn('Keşke şifre daha uzun olsaydı.');
+// Tüm yazılan kodlar silmek için Ekran içinde 
+console.clear();
+
+// Tablo görünümü yapmak için
+console.table({ad:"Emre", yas:31});
+
+// time (ve) timeEnd 2 kod arasında kaç sn geçti onu görmemizi sağlar.
+
+console.time("id");
+
+console.log("aaaaaa");
+console.log("aaaaaa");
+// id:015
+console.timeEnd("id");
+
+// Size mesaj verir.
+let sayi1 = 50;
+
+/*alert(sayi1);*/
+
+// Kullanıcıdan bilgi almak için ekrandan alır. 
+let girilenSayi = prompt("Bir sayi giriniz:");
+
+console.log(girilenSayi);
