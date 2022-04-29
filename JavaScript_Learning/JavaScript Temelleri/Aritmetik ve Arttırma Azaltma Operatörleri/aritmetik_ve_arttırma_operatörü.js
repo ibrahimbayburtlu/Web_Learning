3a67d8dc336548bd099e44ac9b5787ed2a0c55e7
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-04-29 18:00:37 +0300

    I'm learning JavaScript Aritmetik ve Arttırma Azaltma Operatörleri

---
+++ JavaScript_Learning/JavaScript Temelleri/Aritmetik ve Arttırma Azaltma Operatörleri/aritmetik_ve_arttırma_operatörü.js
@@ -0,0 +1,59 @@
+// Aritmetik operatörleri + - * / %
+
+let sayi1 = 10;
+let sayi2 = 2;
+
+// Toplama işlem amacıyla kullanılır
+let sonuc = sayi1 + sayi2;
+console.log(sonuc);
+
+// Çıkarma işlem amacıyla kullanılır
+sonuc = sayi1 - sayi2;
+console.log(sonuc);
+
+// Çarpma işlem amacıyla kullanılır
+sonuc = sayi1 * sayi2;
+console.log(sonuc);
+
+// Bölme işlemi amacıyla kullanılır
+sonuc = sayi1 / sayi2;
+console.log(sonuc);
+
+// Kalan bulma amacıyla kullanılır. 
+sonuc = sayi1 % sayi2 ;
+console.log(sonuc);
+
+// NaN değeri döndürür.
+let isim = "ibrahim";
+let soyİsim = "Bayburtlu";
+let tamİsim = isim - soyİsim;
+console.log(tamİsim);
+
+// Sayı + Metin toplama yapar.
+let yas = 30;
+let metinOlarakyas = " 30";
+console.log(yas + metinOlarakyas);
+
+// Arttırma ve Azaltma Operatörleri
+
+sayi1 = sayi1 + 1;
+console.log(sayi1);
+sayi1++;
+console.log(sayi1); // sayi1 = sayi1 + 1 ile aynı mantık 
+
+// önce değişken sonra azalt.
+sayi1 = sayi1 -1;
+console.log(sayi1);
+sayi1--;
+console.log(sayi1);
+
+// önce arttır sonra atama yap.
+console.log("Sayi 2:",++sayi2);
+
+let sayi3 = 20;
+console.log(sayi3++);
+console.log(sayi3);
+
+let sayi4 = 20;
+console.log(++sayi4);
+console.log(sayi4);
