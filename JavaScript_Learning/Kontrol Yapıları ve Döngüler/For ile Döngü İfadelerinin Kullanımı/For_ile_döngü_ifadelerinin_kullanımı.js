e91ef8ce76ce55a04be02ec08390df69faeebad1
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-04-30 21:39:41 +0300

    For ile Döngü İfadelerinin Kullanımı

---
+++ JavaScript_Learning/Kontrol Yapıları ve Döngüler/For ile Döngü İfadelerinin Kullanımı/For_ile_döngü_ifadelerinin_kullanımı.js
@@ -0,0 +1,25 @@
+// For döngüsü
+// for (ilk atama; döngü_çalıştırma ; her_çalışma_ne_olacak)
+
+
+for(let i = 0; i < 20; i++){
+    console.log("Benim adim ibrahim");
+    console.log("Selam ben ibrahim");
+}
+
+for(let i = 0, j = 0; (i+j) < 20; i+=5 , j+=2){
+    console.log("Döngü çalıştı");
+    console.log("i değeri:",i+" j değeri:",j);
+}
+
+// Sonsuz Döngü oluşturulur.
+/*
+for(; ;){
+
+}
+*/
+// For dan sonra ; olmaz.
+for (let i = 0; i < 5; i++){
+    console.log("Merhaba "+i);
+}
+
