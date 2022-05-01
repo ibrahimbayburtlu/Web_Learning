f253b2219abe67ac4a483d17e3639c7b58a12cb3
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-05-01 21:31:30 +0300

    4.Soru

---
+++ JavaScript_Learning/Genel Tekrar Ve Sorularla Konuların Pekiştirilmesi/4.Soru/soru4.js
@@ -0,0 +1,10 @@
+/* Kendi adınızı ekrana yazdıran uygulamayı tüm döngülerle yazdırınız.*/
+
+for (let i = 0; i < 5; i++){
+    console.log("İbrahim\n");
+}
+let i = 0; 
+while(i < 5){
+    console.log("İbrahim");
+    i++;
+}
