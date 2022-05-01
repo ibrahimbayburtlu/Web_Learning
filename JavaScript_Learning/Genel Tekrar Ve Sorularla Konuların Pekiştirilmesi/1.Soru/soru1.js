e4070658b64f8b23b734a38cdf9af3755115191b
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-05-01 21:09:27 +0300

    1.Soru

---
+++ JavaScript_Learning/Genel Tekrar Ve Sorularla Konuların Pekiştirilmesi/1.Soru/soru1.js
@@ -0,0 +1,12 @@
+/* Klavyeden girilen 3 sayının ortalamasını alan bir uygulama yazınız.*/
+
+
+let num1 = parseInt(prompt("1.sayi giriniz:"));
+
+let num2 = parseInt(prompt("2.sayi giriniz:"));
+
+let num3 =parseInt(prompt("3.sayi giriniz:"));
+
+let ortalama = (num1 + num2 + num3) / 3;  
+
+console.log("Klavyeden girilen 3 sayının ortalaması:",parseInt(ortalama));
