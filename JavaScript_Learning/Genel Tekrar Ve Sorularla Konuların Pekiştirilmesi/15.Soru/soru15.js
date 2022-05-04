f0a16b12b8ae74f29d5c45ced7be03f11ca57438
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-05-04 22:26:28 +0300

    15.Soru

---
+++ JavaScript_Learning/Genel Tekrar Ve Sorularla Konuların Pekiştirilmesi/15.Soru/soru15.js
@@ -0,0 +1,8 @@
+/* Soru 15: 1 + 2 + 4 + 7 + 11 + 16 + 20 + 23 + 25 + 28 + 31 + 35 = 229
+işleminin sonucunu bulan js kodunu yazın. */
+
+let toplam = 0;
+
+toplam = 1 + 2 + 4 + 7 + 11 + 16 + 20 + 23 + 25 + 28 + 31 + 35;
+
+console.log(`toplam:${toplam}`);
