b8645d44a6e903559395723e76a59dd125617862
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-05-04 21:29:45 +0300

    10.Soru

---
+++ JavaScript_Learning/Genel Tekrar Ve Sorularla Konuların Pekiştirilmesi/10.Soru/soru10.js
@@ -0,0 +1,10 @@
+/* Soru 10: Çarpım tablosunu oluşturan bir uygulama yazınız.*/
+
+
+for (let i = 1 ; i < 11; i++){
+
+    for(let j = 1 ; j < 11; j ++)
+        console.log(`${j} x ${i} = ${j*i}`);
+    
+    console.log("\n");
+}
