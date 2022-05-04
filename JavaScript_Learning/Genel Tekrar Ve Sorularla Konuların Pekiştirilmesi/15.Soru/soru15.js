4e9e324932896d8fc90cbea4f403662c96bcef53
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-05-04 22:28:04 +0300

    15.Soru

--- JavaScript_Learning/Genel Tekrar Ve Sorularla Konuların Pekiştirilmesi/15.Soru/soru15.js
+++ JavaScript_Learning/Genel Tekrar Ve Sorularla Konuların Pekiştirilmesi/15.Soru/soru15.js
@@ -1,4 +1,4 @@
-/* Soru 15: 1 + 2 + 4 + 7 + 11 + 16 + 20 + 23 + 25 + 28 + 31 + 35 = 229
+/* Soru 15: 1 + 2 + 4 + 7 + 11 + 16 + 20 + 23 + 25 + 28 + 31 + 35 = 203
 işleminin sonucunu bulan js kodunu yazın. */
 
 let toplam = 0;
