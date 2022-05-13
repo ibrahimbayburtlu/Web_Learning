e27f5ac9e8069812c56b1bf324c00afa978ffbd8
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-05-13 17:58:14 +0300

    reduce fonksiyonu ile ilgili uzmanlık sorusu

---
+++ JavaScript_Learning/Higher Order  Callback Functions ve Array Helper Metodları/reduce fonksiyonu ile ilgili uzmanlık sorusu/reduce_fonksiyonu_ilgili.js
@@ -0,0 +1,20 @@
+function parantezDengeliMi(metin){
+    const dizi = metin.split('');
+
+    const sonuc = dizi.reduce(function (previous,karakter){
+
+        if(previous < 0)
+            return previous;
+        if(karakter === '(')
+            return ++previous;
+        if(karakter === ')')
+            return --previous;
+        return previous;
+    },0);
+    if(!sonuc)
+        console.log(metin + "dengeli bir yapıdadır.");
+    else
+        console.log(metin + "dengeli bir yapıda değildir.");
+}
+
+parantezDengeliMi('())')
