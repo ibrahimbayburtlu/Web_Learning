f6984b8bd0e7e5ca8a8378e0a3f23d23a1d289cb
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-05-18 20:35:07 +0300

    Dom Nedir

---
+++ JavaScript_Learning/Dom Kavramı ve Eventler/Dom Nedir Genel Kullanımı/dom.js
@@ -0,0 +1,30 @@
+let deger ;
+
+deger = window;
+
+deger = window.document;
+
+deger = document.all;
+
+// deger = document.all.length;
+
+
+let dizi = Array.from(deger);
+
+/*
+dizi.forEach(function(item){
+    console.log(item);
+});
+*/
+
+
+deger = document.head;
+deger = document.body;
+deger = document.domain;
+deger = document.URL;
+deger = document.contentType;
+
+deger = document.forms;
+
+
+console.log(deger);
