a8f271165f7855031c1f81fbe3cd79cb40aca3f2
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-05-29 15:53:25 +0300

    JavaScript

---
+++ ITÜ Blockchain/ex4.js
@@ -0,0 +1,13 @@
+function writeChars(char,n){
+    for(let i = 0; i < n; i++){
+        document.write(char);
+    }
+}
+for(let i = 0; i < 10; i++){
+    if(i % 2 == 0){
+        writeChars('*',i);
+    }else{
+        writeChars("a",2);
+    }
+    document.write("<br/>");
+}
