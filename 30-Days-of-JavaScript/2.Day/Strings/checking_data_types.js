0386f0286201d481b0488773354f33170212c9e2
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-07-29 21:52:47 +0300

    2.Day checking_data_types

---
+++ 30-Days-of-JavaScript/2.Day/Strings/checking_data_types.js
@@ -0,0 +1,22 @@
+// Different javascript data types
+// Let's declare different data types
+
+
+
+console.log(typeof 'Asabeneh')  // string
+console.log(typeof firstName)   // string
+console.log(typeof 10)          // number
+console.log(typeof 3.14)        // number
+console.log(typeof true)        // boolean
+console.log(typeof false)       // boolean
+console.log(typeof NaN)         // number
+console.log(typeof job)         // undefined
+console.log(typeof undefined)   // undefined
+console.log(typeof null)        // object
+
+// String to Int (parseInt / number())
+
+let num = '10'
+let numInt = parseInt(num)
+console.log(numInt) // 10
+
