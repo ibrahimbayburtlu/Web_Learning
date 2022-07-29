3d9e0400454f136b974541132b3d3674691ccdce
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-07-29 17:08:28 +0300

    1.Day

---
+++ 30-Days-of-JavaScript/1.Day/datatypes.js
@@ -0,0 +1,9 @@
+let stringVariable = "Hello World!"
+let booleanVariable = true
+let undefinedVariable 
+let nullVariable = null
+
+console.log(typeof stringVariable)
+console.log(typeof booleanVariable)
+console.log(typeof undefinedVariable)
+console.log(typeof nullVariable)
