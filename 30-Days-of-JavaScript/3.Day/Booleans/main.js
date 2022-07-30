48c3681fbd23e8e3c8c966fe8e8a9120679f1fe8
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-07-30 12:30:37 +0300

    3.Day Booleans

---
+++ 30-Days-of-JavaScript/3.Day/Booleans/main.js
@@ -0,0 +1,13 @@
+let isLightOn = true
+let isRaining = false
+let isHungry = false
+let isMarried = true
+let trueValue = 4 > 3 // True
+let falseValue = 4 < 3  // False
+
+console.log(isLightOn)
+console.log(isRaining)
+console.log(isHungry)
+console.log(isMarried)
+console.log(trueValue)
+
