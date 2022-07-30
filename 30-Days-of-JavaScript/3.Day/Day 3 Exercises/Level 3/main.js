3536234e35d441394eb6b4098b089ff37b9c8e1c
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-07-30 15:35:26 +0300

    3.Day Exercises Level 3

---
+++ 30-Days-of-JavaScript/3.Day/Day 3 Exercises/Level 3/main.js
@@ -0,0 +1,8 @@
+let now = new Date
+
+let nowYear = now.getFullYear()
+let nowMonth = now.getMonth()
+let nowDay = now.getDay()
+let nowHours = now.getHours()
+let nowMin = now.getMinutes()
+window.alert(nowYear + "-"+nowMonth + "-" + nowDay + " " +nowHours+":"+nowMin)
