535356b9eb4002cd103d7f117a6aeea630e4ac66
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-07-30 13:59:36 +0300

    3.Day Exercise Level 1

---
+++ 30-Days-of-JavaScript/3.Day/Day 3 Exercises/Level 1/main.js
@@ -0,0 +1,81 @@
+// 1.Declare firstName, lastName, country, city, age, isMarried, year variable and assign value to it and use the typeof operator to check different data types
+let firstName = "ibrahim"
+console.log(typeof firstName)
+
+let lastName = "lastName"
+console.log(typeof firstName)
+
+let country = "Turkey"
+console.log(typeof firstName)
+
+let city = "Bayburt"
+console.log(typeof firstName)
+
+let age = 21
+console.log(typeof firstName)
+
+let isMarried = "No"
+console.log(typeof firstName)
+
+console.log(`My name ${firstName} ${lastName}. My country ${country} and my city ${city}. My age ${age} and married station: ${isMarried}`)
+
+// 2.Check if type of '10' is equal to 10
+console.log('10' === 10)
+
+// 3.Check if parseInt('9.8') is equal to 10
+
+console.log(parseInt(9.8) == 10)
+
+// 4.Boolean value is either true or false.
+
+console.log(true)
+console.log(false)
+
+// 5.Figure out the result of the following comparison expression first without using console.log(). After you decide the result confirm it using console.log()
+
+console.log(4 > 3)
+console.log(4 >= 3)
+console.log(4 < 3)
+console.log(4 <= 3)
+console.log(4 == 4)
+console.log(4 === 4)
+console.log(4 !== 4)
+console.log(4 != 4)
+console.log(4 !== 4)
+console.log(4 !='4')
+console.log(4 == '4')
+console.log(4 === '4')
+console.log('python'.length == 'jargon'.length)
+
+// 6.Figure out the result of the following expressions first without using console.log(). After you decide the result confirm it by using console.log()
+
+console.log(4 > 3 && 10 < 12)
+console.log(4 > 3 && 10 > 12)
+console.log(4 > 3 || 10 < 12)
+console.log(4 > 3 || 10 > 12)
+console.log(4 <= 3)
+console.log(4 >= 3)
+console.log(!(false))
+console.log(!(4 > 3 && 10 < 12))
+console.log(!(4 > 3 && 10 > 12))
+console.log(4 !== '4')
+
+//  7.Use the Date object to do the following activities
+
+let now = new Date
+
+console.log(now.getFullYear())
+
+console.log(now.getMonth())
+
+console.log(now.getDay())
+
+console.log(now.getDay())
+
+console.log(now.getHours())
+
+console.log(now.getMinutes())
+
+console.log(now.getTime())
+
+
