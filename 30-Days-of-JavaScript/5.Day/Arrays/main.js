f0e0418a661c38c830cad6c9651f7e9feaa2190f
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-07-31 21:18:10 +0300

    5.Day Create Arrays

---
+++ 30-Days-of-JavaScript/5.Day/Arrays/main.js
@@ -0,0 +1,12 @@
+// How to create an empty array
+
+// syntax
+const arr = Array()
+// or
+// let arr = new Array()
+console.log(arr) // []
+
+// Using square brackets([])
+
+let arr2 = []
+console.log(arr2)
