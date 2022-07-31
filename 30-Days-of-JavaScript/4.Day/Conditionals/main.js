075c8589722955d20e99a1a5fc84041b87c1c57f
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-07-31 12:22:47 +0300

    4.Day Conditionals

--- 30-Days-of-JavaScript/4.Day/Conditionals/main.js
+++ 30-Days-of-JavaScript/4.Day/Conditionals/main.js
@@ -61,3 +61,66 @@ if (isRaining) {
 }
 // No need for a rain coat.
 
+let a = 0
+if (a > 0) {
+  console.log(`${a} is a positive number`)
+} else if (a < 0) {
+  console.log(`${a} is a negative number`)
+} else if (a == 0) {
+  console.log(`${a} is zero`)
+} else {
+  console.log(`${a} is not a number`)
+}
+
+
+// Switch
+
+let weather = 'cloudy'
+switch (weather) {
+  case 'rainy':
+    console.log('You need a rain coat.')
+    break
+  case 'cloudy':
+    console.log('It might be cold, you need a jacket.')
+    break
+  case 'sunny':
+    console.log('Go out freely.')
+    break
+  default:
+    console.log(' No need for rain coat.')
+}
+
+// Switch More Examples
+let dayUserInput = str(prompt('What day is today ?'))
+let day = dayUserInput.toLowerCase()
+
+switch (day) {
+  case 'monday':
+    console.log('Today is Monday')
+    break
+  case 'tuesday':
+    console.log('Today is Tuesday')
+    break
+  case 'wednesday':
+    console.log('Today is Wednesday')
+    break
+  case 'thursday':
+    console.log('Today is Thursday')
+    break
+  case 'friday':
+    console.log('Today is Friday')
+    break
+  case 'saturday':
+    console.log('Today is Saturday')
+    break
+  case 'sunday':
+    console.log('Today is Sunday')
+    break
+  default:
+    console.log('It is not a week day.')
+}
+
+// Ternary Operators
+
+isRaining ? console.log('You need a rain coat.') : console.log('No need for a rain coat.')
+
