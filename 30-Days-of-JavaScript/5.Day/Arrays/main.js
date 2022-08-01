49b27ded47a4c6ba44ef1e76c1dca2c8e56975c3
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-08-01 13:43:07 +0300

    5.Day Creating an array using split

--- 30-Days-of-JavaScript/5.Day/Arrays/main.js
+++ 30-Days-of-JavaScript/5.Day/Arrays/main.js
@@ -38,3 +38,14 @@ console.log('Number of web technologies:', webTechs.length)
 console.log('Countries:', countries)
 console.log('Number of countries:', countries.length)
 
+
+let js = 'JavaScript'
+
+const charsInJavaScript = js.split('')
+
+console.log(charsInJavaScript)
+
+let companiesString = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+const companies = companiesString.split(',')
+
+console.log(companies)
