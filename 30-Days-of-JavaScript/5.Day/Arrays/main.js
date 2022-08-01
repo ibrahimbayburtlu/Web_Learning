0f104ca361b6a6d080d8911c3a647d67fbcc954f
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2022-08-01 13:53:41 +0300

    5.Day Arrays

--- 30-Days-of-JavaScript/5.Day/Arrays/main.js
+++ 30-Days-of-JavaScript/5.Day/Arrays/main.js
@@ -49,3 +49,62 @@ let companiesString = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
 const companies = companiesString.split(',')
 
 console.log(companies)
+
+let firstFruit = fruits[0] // we are accessing the first item using its index
+
+console.log(firstFruit) // banana
+
+let secondFruit = fruits[1]
+console.log(secondFruit) // orange
+
+let lastFruit = fruits[3]
+console.log(lastFruit) // lemon
+// Last index can be calculated as follows
+
+let lastIndex = fruits.length - 1
+lastFruit = fruits[lastIndex]
+
+console.log(lastFruit)  // lemon
+
+// Creating static values with fill
+
+// creates an an empty array
+console.log(arr)
+
+const eightXvalues = Array(8).fill('X') // it creates eight element values filled with 'X'
+console.log(eightXvalues) // ['X', 'X','X','X','X','X','X','X']
+
+const eight0values = Array(8).fill(0) // it creates eight element values filled with '0'
+console.log(eight0values) // [0, 0, 0, 0, 0, 0, 0, 0]
+
+const four4values = Array(4).fill(4) // it creates 4 element values filled with '4'
+console.log(four4values) // [4, 4, 4, 4]
+
+const firstList = [1, 2, 3]
+const secondList = [4, 5, 6]
+const thirdList = firstList.concat(secondList)
+
+console.log(thirdList) // [1, 2, 3, 4, 5, 6]
+
+// let us check if a banana exist in the array
+
+let index = fruits.indexOf('banana')  // 0
+
+if(index === -1){
+   console.log('This fruit does not exist in the array')  
+} else {
+    console.log('This fruit does exist in the array')
+}
+// This fruit does exist in the array
+
+// we can use also ternary here
+index === -1 ? console.log('This fruit does not exist in the array'): console.log('This fruit does exist in the array')
+
+// let us check if an avocado exist in the array
+let indexOfAvocado = fruits.indexOf('avocado')  // -1, if the element not found index is -1
+if(indexOfAvocado === -1){
+   console.log('This fruit does not exist in the array')  
+} else {
+    console.log('This fruit does exist in the array')
+}
+// This fruit does not exist in the array
